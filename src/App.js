432581b1aa796b8e49aa5b13ab1db77eab6e2f0f
bilal-uddin <bilaluddinwardak@gmail.com>
2023-03-20 20:47:16 +0100

    test file is ready

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>this is updated code</h1>
+        <h2>this is test file</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
